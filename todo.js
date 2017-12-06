17085e2218892011b0ef7ab142a45c995933e266
oops-accidental <smp5119@gmail.com>
2017-12-05 19:34:48 -0800

    added ability to remove items from the list

--- todo.js
+++ todo.js
@@ -17,19 +17,21 @@ function submitForm(event) {
 function changeCheckbox(event) {
     if (event.target.matches('input[type="checkbox"]')) {
         event.target.parentElement.className = event.target.checked ? 'completed' : 'incomplete'
-        //Change tag/class to make it easier to hide them
-        //event.target.parentElement.tag = event.target.checked ? 'completed' : 'incomplete'
     }
 }
 
 function toggleItemVisibility(event) {
     if (event.target.matches('button')) {
+        if (event.target.innerText === 'X') {
+            event.target.parentElement.parentElement.remove()
+            return
+        }
         showElementsOfAllClasses()
         console.log(event.target.innerText)
         if (event.target.innerText === 'Completed') {
-            hideElementsOfClass('incomplete')
+            toggleElementsOfClass('incomplete', false)
         } else if (event.target.innerText === 'Incomplete') {
-            hideElementsOfClass('completed')
+            toggleElementsOfClass('completed', false)
         }
     }
 }
@@ -41,22 +43,11 @@ function showElementsOfAllClasses() {
 
 function toggleElementsOfClass(className, toggleOn) {
     const displayType = toggleOn ? 'block' : 'none'
-    var divsToHide = document.body.getElementsByClassName(className)
-    if (divsToHide.length <= 0) {
+    var divs = document.body.getElementsByClassName(className)
+    if (divs.length <= 0) {
         return
     }
-    for (var i = 0; i < divsToHide.length; i++) {
-        divsToHide[i].style.display = displayType
-    }
-}
-
-function hideElementsOfClass(className) {
-    console.log('looking for class ' + className)
-    var divsToHide = document.body.getElementsByClassName(className)
-    if (divsToHide.length <= 0) {
-        return
-    }
-    for (var i = 0; i < divsToHide.length; i++) {
-        divsToHide[i].style.display = 'none'
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].style.display = displayType
     }
 }
